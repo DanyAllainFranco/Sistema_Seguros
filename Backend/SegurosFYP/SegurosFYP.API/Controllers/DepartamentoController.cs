3f9910a52c7b3056d4990f8cfb699ddea6860cd6
Dany <145422491+DanyFranc0@users.noreply.github.com>
2024-04-03 02:33:14 -0600

    Amen

--- Backend/SegurosFYP/SegurosFYP.API/Controllers/DepartamentoController.cs
+++ Backend/SegurosFYP/SegurosFYP.API/Controllers/DepartamentoController.cs
@@ -1,18 +1,15 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using SegurosFYP.BusinessLogic.Services;
-using SegurosFYP.Common;
 using SegurosFYP.Common.Models;
 using SegurosFYP.Entities.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SegurosFYP.API.Controllers
 {
     [ApiController]
-    public class DepartamentoController : Controller
+    [Route("api/[controller]")]
+    public class DepartamentoController : ControllerBase
     {
         private readonly GeneralServices _generalServices;
         private readonly IMapper _mapper;
@@ -22,26 +19,20 @@ namespace SegurosFYP.API.Controllers
             _generalServices = generalServices;
             _mapper = mapper;
         }
+
         [HttpGet("List/Departamentos")]
-        public IActionResult Index()
+        public IActionResult ListDepartamentos()
         {
             var list = _generalServices.ListDepar();
             return Ok(list);
         }
 
         [HttpPost("Insert/Departamentos")]
-        public IActionResult Insert(DepartamentoViewModel item)
+        public IActionResult InsertDepartamento(DepartamentoViewModel item)
         {
             var model = _mapper.Map<tbDepartamentos>(item);
-            var modelo = new tbDepartamentos()
-            {
-                Depar_Id  = item.Depar_Id,
-                Depar_Descripcion = item.Depar_Descripcion,
-                Depar_UsuarioCreacion = item.Depar_UsuarioCreacion,
-                Depar_FechaCreacion = item.Depar_FechaCreacion
-            };
-            var list = _generalServices.InsertDepar(modelo);
-            return Ok(list);
+            var result = _generalServices.InsertDepar(model);
+            return Ok(result);
         }
     }
 }
---
+++ Frontend/SegurosFYP/SegurosFYP/Models/DepartamentoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SegurosFYP.Models
+{
+    public class DepartamentoViewModel
+    {
+        [Display(Name = "Codigo")]
+        public string Depar_Id { get; set; }
+        [Display(Name = "Departamento")]
+        public string Depar_Descripcion { get; set; }
+        [Display(Name = "Usuario Creacion")]
+        public int Depar_UsuarioCreacion { get; set; }
+        [Display(Name = "Fecha Creacion")]
+        public DateTime Depar_FechaCreacion { get; set; }
+        [Display(Name = "Usuario Modificacion")]
+        public int? Depar_UsuarioModificacion { get; set; }
+        [Display(Name = "Usuario Modificacion")]
+        public DateTime? Depar_FechaModificacion { get; set; }
+        public bool? Depar_Estado { get; set; }
+
+    }
+}
